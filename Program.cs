1e589169f155313dd96153ce909f3f5831476208
Nino Floris <mail@ninofloris.com>
2017-07-19 13:14:59 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using StructureMap;
+
+namespace repro_child_container_ctorpolicy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var container = new Container().CreateChildContainer(); // Fails
+            // var container = new Container(); // Works
+
+            return WebHost.CreateDefaultBuilder(args)
+                .ConfigureServices(svc =>
+                {
+                    svc.AddMvcCore();
+
+                    // StructureMap.Microsoft.DependencyInjection
+                    // Populate runs registry.Policies.ConstructorSelector<AspNetConstructorSelector>();
+                    container.Populate(svc);
+                })
+                .Configure(app =>
+                {
+                    app.ApplicationServices = container.GetInstance<IServiceProvider>();
+                    app.UseMvc();
+                })
+                .Build();
+        }
+
+    }
+}
